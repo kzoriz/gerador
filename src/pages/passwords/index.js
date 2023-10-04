b642b4dd06d391626612368b29015d2022d336ee
kzoriz <kzoriz@outlook.com>
2023-10-04 19:52:49 -0300

    finish

--- src/pages/passwords/index.js
+++ src/pages/passwords/index.js
@@ -1,11 +1,43 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { useState, useEffect } from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useIsFocused } from '@react-navigation/native'
+import useStorage from "../../hooks/useStorage";
+import { PasswordItem } from './component/passwordItem'
 
 export function Passwords(){
+
+    const [listPasswords, setListPasswords] = useState([])
+    const focused = useIsFocused();
+    const { getItem, removeItem } = useStorage();
+
+    useEffect(()=>{
+        async function loadPasswords(){
+            const passwords = await getItem("@pass")
+            //console.log(passwords);
+            setListPasswords(passwords);
+        }
+        loadPasswords().then(r => null);
+    },[focused]);
+
+    async function handleDeletePassword(item){
+        //console.log(item);
+        const passwords = await removeItem("@pass", item)
+        setListPasswords(passwords);
+    }
+
+
     return(
         <SafeAreaView style={{flex: 1, }}>
             <View style={styles.header}>
                 <Text style={styles.title}>Minhas Senhas</Text>
+            </View>
+            <View style={styles.content}>
+                <FlatList style={{flex:1, paddingTop: 14}}
+                    data={listPasswords}
+                    keyExtractor={ (item) =>String(item)}
+                    renderItem={({item})=> <PasswordItem data={item} removePassword={() => handleDeletePassword(item) } />}
+                        />
             </View>
         </SafeAreaView>
     )
@@ -13,18 +45,20 @@ export function Passwords(){
 
 const styles = StyleSheet.create({
     header:{
-        // flex:1,
         backgroundColor: '#392de9',
         paddingTop: 58,
         paddingBottom: 14,
         paddingLeft: 14,
         paddingRight: 14,
-        // justifyContent: 'center',
-        // alignItems: 'center'
     },
     title:{
         fontSize: 18,
         color: '#fff',
         fontWeight: 'bold'
+    },
+    content:{
+        flex:1,
+        paddingRight: 14,
+        paddingLeft: 14,
     }
 })
